e88391a94e412c244839e03cebc72647c6e16f4a
Philip Young <mainframed767@gmail.com>
2025-02-15 17:32:06 -0800

    lest try this out

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import DiskCalculator from '../components/DiskCalculator'
+
+export default function Home() {
+  return (
+    <main className="p-4">
+      <DiskCalculator />
+    </main>
+  )
+}
---
+++ components/ui/label.tsx
@@ -0,0 +1,10 @@
+// components/ui/label.tsx
+export function Label({ className, ...props }: React.LabelHTMLAttributes<HTMLLabelElement>) {
+    return (
+      <label
+        className={`text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${className}`}
+        {...props}
+      />
+    )
+  }
+  
