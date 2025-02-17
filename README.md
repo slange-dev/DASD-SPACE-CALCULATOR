68131ffd1696cbcd6549e8951b5f69189ee1230e
Philip Young <mainframed767@gmail.com>
2025-02-17 08:24:31 -0800

    clearing out repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# SPACE
-
-The final frontier... for mainframes. 
-
-This is a website you can use to calculate space based on various DASD types. 
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DiskCalculator from '../components/DiskCalculator'
-
-export default function Home() {
-  return (
-    <main className="p-4">
-      <DiskCalculator />
-    </main>
-  )
-}
